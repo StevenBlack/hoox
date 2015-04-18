a1f370a56d3e74939740a237d6c717968d0032f3
Steven Black <steveb@stevenblack.com>
2015-04-17 21:18:31 -0400

    Style - removing extra spaces within parenthesis.

--- hooks.rb
+++ hooks.rb
@@ -8,28 +8,28 @@ class AbstractHook
   # The @oHook member is designed to hold other instances of this class,
   # thus making all hooks potentially self-referential
 
-  @oHook =  nil
+  @oHook = nil
 
   ##
   # The main interface for hooks.  The process method delegates to this
   # instance's execute method, then delegates down the hook chain.
 
-  def process( *args )
-    if preprocess( args )
-      execute( args )
+  def process(*args)
+    if preprocess(args)
+      execute(args)
     end
 
     if ! @hook.nil?
-      @hook.process( args )
+      @hook.process(args)
     end
-    postprocess( args )
+    postprocess(args)
   end
 
   ##
   # This method fires before the execute method and ultimately determines
   # if the execute method fires at all.
 
-  def preprocess( *args )
+  def preprocess(*args)
     # Override to implement whether this hook executes
     true
   end
@@ -38,14 +38,13 @@ class AbstractHook
   # This method fires after the hook chain processes returns.  This is a good
   # place for cleanup code.
 
-  def postprocess( *args )
-
+  def postprocess(*args)
   end
 
   ##
   # This is where the meat of the hook lives.
 
-  def execute( *args )
+  def execute(*args)
     # Implement the hook here
   end
 
@@ -54,17 +53,16 @@ class AbstractHook
   # reference here.  If the hook member is not nil, delegate the hook to its
   # sethook method.
 
-  def sethook( *args )
+  def sethook(*args)
     args.each { |arg|
-
-      arg = resolve_to_hook( arg )
+      arg = resolve_to_hook(arg)
 
       case
       when arg.nil?    # do nothing
       when @hook.nil?
         @hook = arg    # hook stays here
       else
-        @hook.sethook( arg )   # hook is appended to the chain
+        @hook.sethook(arg)   # hook is appended to the chain
       end
     }
   end
@@ -73,14 +71,14 @@ class AbstractHook
   # Pass it a hook instance or a string and, in the case of string, creates a
   # hook of that class bame.
 
-  def resolve_to_hook( arg )
+  def resolve_to_hook(arg)
     ret = nil
     case
-    when arg.is_a?( AbstractHook )
+    when arg.is_a?(AbstractHook)
       ret = arg
-    when arg.is_a?( String ) && Kernel.const_defined?( arg )
-      tmp = Kernel.const_get( arg ).new
-      if tmp.is_a?( AbstractHook )
+    when arg.is_a?(String) && Kernel.const_defined?(arg)
+      tmp = Kernel.const_get(arg).new
+      if tmp.is_a?(AbstractHook)
         ret = tmp
       end
     end
@@ -96,21 +94,20 @@ end
 class HookAnchor < AbstractHook
   attr_accessor :ahook
 
-  def initialize( *args )
+  def initialize(*args)
     self.ahook = [] # on object creation initialize this to an array
     args.each { |arg|
-
-      arg = resolve_to_hook( arg )
+      arg = resolve_to_hook(arg)
       if ! arg.nil?
         self.ahook << arg
       end
     }
   end
 
-  def execute( *args )
+  def execute(*args)
     @ahook.each { | hook |
-      if hook.is_a?( AbstractHook )
-        hook.process( args )
+      if hook.is_a?(AbstractHook)
+        hook.process(args)
       end
     }
   end
