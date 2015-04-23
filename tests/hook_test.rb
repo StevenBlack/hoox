7dbeb4ffba0ec962afd651b847e4362fd6837bd2
Steven Black <steveb@stevenblack.com>
2015-04-23 00:52:19 -0400

    Expand scope of tests,

--- tests/hook_test.rb
+++ tests/hook_test.rb
@@ -4,11 +4,27 @@ require './hooks'
 class TestAnchor < Minitest::Test
 
   def setup
+    @vanillahook   = AbstractHook.new
     @vanillaanchor = HookAnchor.new
   end
 
-  def test_ahook_member_is_array
-    assert_kind_of Array, @vanillaanchor.ahook, "Member ahook is not Array"
+  # VANILLA HOOKS
+  # A process call must not mutate the argument
+  def test_hook_process_call_does_not_mutate
+  	teststring = "Short, sharp shocked"
+    assert_equal( teststring, @vanillahook.process(teststring), "Vanilla hooks must not mutate arguments")
+  end
+
+
+  # VANILLA ANCHOR
+  # The Anchor.ahook member must be an array
+  def test_anchor_ahook_member_is_array
+    assert_kind_of( Array, @vanillaanchor.ahook, "Member ahook is not Array" )
+  end
+  # A process call must not mutate the argument
+  def test_anchor_process_call_does_not_mutate
+  	teststring = "Short, sharp shocked"
+    assert_equal( teststring, @vanillaanchor.process(teststring), "Vanilla hooks must not mutate arguments")
   end
 
 end
