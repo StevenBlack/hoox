0306405f855ddea9a3d88a9307f59fabd8ccf2ea
Steven Black <steveb@stevenblack.com>
2015-04-24 09:36:36 -0400

    New test: last hook should 'win'.

--- tests/trivialparserhook_test.rb
+++ tests/trivialparserhook_test.rb
@@ -5,13 +5,22 @@ class TestAnchor < Minitest::Test
 
   def setup
     @lowerhook   = LowerHook.new
+    @upperhook   = UpperHook.new
   end
 
   # VANILLA HOOKS
   # A process call must not mutate the argument
   def test_lowerhook
-  	teststring = "Short, sharp shocked"
+  	teststring = "Short, sharp, shocked"
     assert_equal( teststring.downcase, @lowerhook.process(teststring), "Functional error in class LowerHook")
+  end
+
+  # Last hook wins
+  def test_last_hook_wins
+  	teststring = "Short, sharp, shocked"
+  	@lowerhook.sethook( @upperhook )
+  	result1 = teststring.downcase.upcase
+    assert_equal( result1, @lowerhook.process(teststring), "Last hook should 'win'")
   end
 
 end
--- trivialparserhooks.rb
+++ trivialparserhooks.rb
@@ -4,6 +4,13 @@ require './hooks'
 class LowerHook < AbstractHook
   # Public: The main method for ParserHooks.  We assume the argument is text.
   def execute(arg)
-    arg.downcase
+    arg.downcase!
+  end
+end
+# Public: A trivial parser hook
+class UpperHook < AbstractHook
+  # Public: The main method for ParserHooks.  We assume the argument is text.
+  def execute(arg)
+    arg.upcase!
   end
 end
