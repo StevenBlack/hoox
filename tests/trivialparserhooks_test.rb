7a1cffcbeb66a1d6c45edf02f1ac8f2bf21e0537
Steven Black <steveb@stevenblack.com>
2015-04-26 23:09:29 -0400

    Rename the test files for consistency.

---
+++ tests/trivialparserhooks_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require './trivialparserhooks.rb'
+
+class TestTrivialHooks < Minitest::Test
+
+  def setup
+    @lower_hook   = LowerHook.new
+    @upper_hook   = UpperHook.new
+  end
+
+  # VANILLA HOOKS
+  # A process call must not mutate the argument
+  def test_lowerhook
+  	test_string = "Short, sharp, shocked"
+    assert_equal( test_string.downcase, @lower_hook.process( test_string ), "Functional error in class LowerHook")
+  end
+
+  # Last hook wins
+  def test_last_hook_wins
+  	test_string = "Short, sharp, shocked"
+  	@lower_hook.sethook( @upper_hook )
+  	result1 = test_string.downcase.upcase
+    assert_equal( result1, @lowerhook.process( test_string ), "Last hook should 'win'")
+  end
+
+end
