daede8e97df3f80a3cc36fe36191fda8ddbce75f
Steven Black <steveb@stevenblack.com>
2015-04-27 00:41:33 -0400

    Evolution of transformation hooks, and tests.

--- tests/transformationhooks_test.rb
+++ tests/transformationhooks_test.rb
@@ -4,12 +4,25 @@ require './transformationhooks.rb'
 class TestTransformations < Minitest::Test
 
   def setup
-    @tab_hide_hook = TabHideHook.new
+    @tab_hide_hook  = TabHideHook.new
+    @html_tag_token = HtmlTagToken.new
   end
 
   # TabHideHook
   def test_tab_hide_hook
   	test_string = "\t\tShort,\tsharp,\tshocked"
     assert_equal( test_string, @tab_hide_hook.process( test_string ), "TabHideHook has side-effects.")
+
+  	test_string = File.read('./tests/assets/sbc.html')
+    assert_equal( test_string, @tab_hide_hook.process( test_string ), "TabHideHook has side-effects.")
   end
+
+  def test_html_tag_token
+    test_string = "\t\tShort,\tsharp,\tshocked"
+    assert_equal( test_string, @html_tag_token.process( test_string ), "TabHideHook has side-effects.")
+
+    test_string = File.read('./tests/assets/sbc.html')
+    assert_equal( test_string, @html_tag_token.process( test_string ), "TabHideHook has side-effects.")
+  end
+
 end
--- transformationhooks.rb
+++ transformationhooks.rb
@@ -1,8 +1,8 @@
 require './hooks'
+require 'securerandom'
 
 # Public: A parser hook to tokenize tab symbols before hook delegation
-class Tabhidehook < ParserHook
-
+class TabHideHook < ParserHook
   def initialize
     # The token to substitite for tabs
     @tab_subst="~!~ "
@@ -10,15 +10,46 @@ class Tabhidehook < ParserHook
 
   # Substiute tabs for tokens
   def preprocess(arg)
-  	arg.gsub!( /\t/, @tab_subst )
+    if arg[/\t/]
+      arg.gsub!( /\t/, @tab_subst )
+    end
+    arg
   end
 
   # Substitute the tokens for tabs
   def postprocess(arg)
-  	arg.gsub!( @tab_subst, "\t" )
+    arg.gsub!( @tab_subst, "\t" )
+    arg
   end
 end
 
+# Oubllic: Hide all HTML tags from downstream hooks
+class HtmlTagToken < ParserHook
+  def initialize
+    # The token to substitite for tabs
+    @matches      = Hash.new
+    @token_prefix = " ((("
+    @token_suffix = "))) "
+  end
 
+  # Remove all HTML tags
+  def preprocess(arg)
+    next_match = arg[/<\/?[^>]*?>/]
+    while next_match
+      next_token = SecureRandom.urlsafe_base64( 20 )
+      next_swap  = @token_prefix + next_token + @token_suffix
+      @matches[ next_token ] = next_match
+      arg.gsub!( next_match, next_swap )
+      next_match = arg[/<\/?[^>]*?>/]
+    end
+    arg
+  end
 
-# <\/?[^>]*?>
+  # Restore all HTML tags
+  def postprocess(arg)
+    @matches.keys.each{ |k|
+      arg.gsub!( @token_prefix + k + @token_suffix, @matches[k] )
+    }
+    arg
+  end
+end
